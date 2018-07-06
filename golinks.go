07a104f1f1dbab68176b8d238eae81fcc39ebe7f
Kirk Scheibelhut <kjs@scheibo.com>
2018-07-05 19:25:17 -0700

    Make golinks compile

--- golinks.go
+++ golinks.go
@@ -1,19 +1,23 @@
 package golinks
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/goware/urlx"
 	"github.com/scheibo/a1"
 	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/html"
 )
 
 type NameLink struct {
@@ -24,30 +28,26 @@ type NameLink struct {
 type Store interface {
 	Get(name string) (string, bool)
 	Set(name, link string) error // Delete(name) => Set(name, "")
-	Iterate(cb func(name, link string) error)
+	Iterate(cb func(name, link string) error) error
 }
 
-func serve(auth *a1.Client, store *Store) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func serve(auth *a1.Client, store Store) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 		switch path {
 		case "/login":
 			switch r.Method {
 			case "GET":
 				auth.CustomLoginPage("favicon.png", fmt.Sprintf("login - %s", r.URL.Host), "/login").ServeHTTP(w, r)
-				return
 			case "POST":
 				auth.Login("/login", "/").ServeHTTP(w, r)
-				return
 			default:
 				httpError(w, 405)
-				return
 			}
 		case "/logout":
 			auth.Logout("/").ServeHTTP(w, r)
-			return
 		default:
-			name := path[1:len(input)]
+			name := path[1:]
 			if !isValidName(name) {
 				httpError(w, 400)
 				return
@@ -55,69 +55,58 @@ func serve(auth *a1.Client, store *Store) *http.Handler {
 			switch r.Method {
 			case "GET":
 				getLink(auth, store, name)
-				return
-			case "POST":
-				auth.CheckXSRF(auth.EnsureAuth(dispatchPost(store, name, r.URL.RawQuery))).ServeHTTP(w, r)
-				return
-			case "UPDATE":
-				auth.CheckXSRF(auth.EnsureAuth(&http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-					link, err := getValidLink(r.URL.Host, r.URL.RawQuery)
-					if err != nil {
-						httpError(w, 400, err)
-					}
-
-					updateLink(store, name, link)
-				}))).ServeHTTP(w, r)
-				return
+			case "POST", "UPDATE":
+				update := r.Method == "UPDATE"
+				auth.CheckXSRF(auth.EnsureAuth(postLink(store, name, update))).ServeHTTP(w, r)
 			case "DELETE":
-				auth.CheckXSRF(auth.EnsureAuth(deleteLink(store, name, r.URL.RawQuery))).ServeHTTP(w, r)
-				return
+				auth.CheckXSRF(auth.EnsureAuth(deleteLink(store, name))).ServeHTTP(w, r)
 			default:
 				httpError(w, 405)
-				return
 			}
 		}
 	})
 }
 
-func getLink(auth *a1.Client, store *Store, name string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func getLink(auth *a1.Client, store Store, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		link, ok := store.Get(name)
 		if !ok {
 			if !auth.IsAuth(r) {
-				http.NotFound(w, req)
+				http.NotFound(w, r)
 				return
 			}
 
 			getIndex(store, auth.XSRF(), name).ServeHTTP(w, r)
-			return
 		}
 		http.Redirect(w, r, link, 302)
 	})
 }
 
-func getIndex(store *Store, token string, name string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+func getIndex(store Store, token string, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var data []NameLink
-		s.Iterate(func(name, link) error {
-			data[name] = link
+		err := store.Iterate(func(name, link string) error {
+			data = append(data, NameLink{Name: name, Link: link})
+			return nil
 		})
+		if err != nil {
+			httpError(w, 500, err)
+			return
+		}
 		t := template.Must(compileTemplates("index.html"))
-		t.Execute(w, struct {
+		_ = t.Execute(w, struct {
 			Title string
 			Token string
 			Name  string
 			Data  []NameLink
 		}{
-			fmt.Sprintf("goto - %s", r.URL.Host), c.XSRF(loginPath), name, data,
+			fmt.Sprintf("goto - %s", r.URL.Host), token, name, data,
 		})
 	})
 }
 
-func dispatchPost(store *Store, name, link, query string) http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// NOTE: we share logic with getValidLink here, because we dispatch differently
-		// if the link param is missing
+func postLink(store Store, name string, update bool) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		values, err := url.ParseQuery(r.URL.RawQuery)
 		if err != nil {
 			httpError(w, 500, err)
@@ -129,37 +118,36 @@ func dispatchPost(store *Store, name, link, query string) http.Handler {
 			return
 		}
 
-		link := values["link"]
+		links, ok := values["link"]
+		if !ok {
+			httpError(w, 400, errors.New("missing link param"))
+		}
+
+		if len(links) > 1 {
+			httpError(w, 400, errors.New("too many link params"))
+			return
+		}
+
+		link := links[0]
 		// Empty or missing link means we attempt to delete.
 		if link == "" {
 			deleteLink(store, name)
 			return
 		}
 
-		link, err := normalizeLink(r.URL.Host, link)
+		link, err = normalizeLink(canonicalizeAlias(store, r.URL.Host, link))
 		if err != nil {
 			httpError(w, 400)
 			return
 		}
 
-		// We don't really care if the link already existed, create
-		// or update at this point do the same thing.
-		err = store.Set(path, link)
-		if err != nil {
-			httpError(w, 500, err)
-			return
-		}
-
-		http.Redirect(w, r, "/", 302)
-	})
-}
-
-func updateLink(store *Store, name, link string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		stored, ok := store.Get(name)
-		if !ok {
-			httpError(w, 404)
-			return
+		// UPDATE should onlyu work on links which already existed
+		if update {
+			_, ok := store.Get(name)
+			if !ok {
+				httpError(w, 404)
+				return
+			}
 		}
 
 		err = store.Set(name, link)
@@ -172,20 +160,20 @@ func updateLink(store *Store, name, link string) *http.Handler {
 	})
 }
 
-func deleteLink(store *Store, name string) *http.Handler {
-	return &http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if http.URL.RawQuery != "" {
+func deleteLink(store Store, name string) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.RawQuery != "" {
 			httpError(w, 400)
 			return
 		}
 
-		stored, ok := store.Get(name)
+		_, ok := store.Get(name)
 		if !ok {
 			httpError(w, 404)
 			return
 		}
 
-		err = store.Set(name, "")
+		err := store.Set(name, "")
 		if err != nil {
 			httpError(w, 500, err)
 			return
@@ -196,9 +184,10 @@ func deleteLink(store *Store, name string) *http.Handler {
 }
 
 // If the link doesn't start with http then check if we can alias it.
-func canonicalizeAlias(host, link string, store *Store) string {
+func canonicalizeAlias(store Store, host, link string) string {
 	if !strings.HasPrefix("http", link) {
-		stored, ok := store.Get(name)
+		link = strings.TrimPrefix(link, "go/")
+		_, ok := store.Get(link)
 		if ok {
 			return fmt.Sprintf("https://%s/%s", host, link)
 		}
@@ -206,26 +195,7 @@ func canonicalizeAlias(host, link string, store *Store) string {
 	return link
 }
 
-func getValidLink(host, query string) (string, error) {
-	values, err := url.ParseQuery(query)
-	if err != nil {
-		return "", err
-	}
-
-	if len(values) != 1 {
-		return "", errors.New("invalid request")
-	}
-
-	link, ok := values["link"]
-	if !ok {
-		return "", errors.New("missing link param")
-	}
-
-	return normalizeLink(host, link)
-}
-
-func normalizeLink(host, link) (string, error) {
-	link := canonicalizeAlias(host, link)
+func normalizeLink(link string) (string, error) {
 	err := errors.New("invalid link")
 	if !isValidLink(link) {
 		return "", err
@@ -250,12 +220,12 @@ func isValidName(name string) bool {
 		// shouldn't be possible anyway, but reject just in case
 		return false
 	}
-	_, err := url.Parse("/" + link)
+	_, err := url.Parse("/" + name)
 	return err != nil
 }
 
 // Must be a valid, absolute URL
-func isValid(link string) bool {
+func isValidLink(link string) bool {
 	u, err := url.Parse(link)
 	if err != nil {
 		return false
@@ -293,7 +263,10 @@ func compileTemplates(filenames ...string) (*template.Template, error) {
 		if err != nil {
 			return nil, err
 		}
-		tmpl.Parse(string(mb))
+		_, err = tmpl.Parse(string(mb))
+		if err != nil {
+			return nil, err
+		}
 	}
 	return tmpl, nil
 }
@@ -305,8 +278,8 @@ func main() {
 
 	flag.StringVar(&file, "file", "", "file for store")
 	flag.StringVar(&hash, "hash", os.Getenv("GOTO_PASSWORD_HASH"), "hash of password")
-	flag.StringVar(&fuzzy, "fuzzy", false, "whether to use fuzzy name semantics")
-	flag.Int64(&port, "port", 8968, "Port")
+	flag.BoolVar(&fuzzy, "fuzzy", false, "whether to use fuzzy name semantics")
+	flag.Int64Var(&port, "port", 8968, "Port")
 
 	flag.Parse()
 
@@ -315,20 +288,22 @@ func main() {
 		os.Exit(1)
 	}
 
-	srv := &http.Server{
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  120 * time.Second,
-	}
-
 	auth := a1.New(hash)
-	store, err := s.Open(file, fuzzy)
+	store, err := Open(file, fuzzy)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println(srv.ListenAndServe(fmt.Sprintf(":%v", port), auth.RateLimit(10, serve(auth))))
-	err := store.Close()
+	srv := &http.Server{
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		Addr:         fmt.Sprintf(":%v", port),
+		Handler:      a1.RateLimit(serve(auth, store), 10),
+	}
+
+	log.Println(srv.ListenAndServe())
+	err = store.Close()
 	if err != nil {
 		log.Fatal(err)
 	}
